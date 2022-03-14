179359b24099fe0922ccff17f61e1ca4edb65f7d
ikatahira <isaque.katahira@fatec.sp.gov.br>
2022-03-14 12:19:38 +0000

    resolucao2

---
+++ Exer2.java
@@ -0,0 +1,14 @@
+public class Exer2{
+    public static void main(String[] args) {
+        
+        System.out.print("nnvdsds");
+        for(int i=1;i<=4;i++){
+
+            if(i%2==0)
+            System.out.print(i);
+            else
+            System.out.print(i);
+
+        }
+    }
+}
